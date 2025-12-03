7aa96e1850b3b99331b4043da5a068de1534652b
taytayyy <taylor.joiner@carvercollegiate.org>
2025-12-03 19:21:23 +0000

    ciurcuit projct

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,11 +20,19 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+            var circle;
+            var circles = [];
+   
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+         function drawCircle() {
+    circle = draw.randomCircleInArea(canvas, true, true,"#999",2);
+    physikz.addRandomVelocity(circle, canvas, 5, 5);
+    view.addChild(circle);
+    circles.push(circle);
+    }
+   
 
 
         // TODO 3 : Call the drawCircle() function
@@ -32,6 +40,11 @@ var init = function (window) {
 
 
         // TODO 7 : Use a loop to create multiple circles
+        
+         for ( var i = 0; i < 100; i++) {
+        drawCircle();
+    }
+   
 
 
 
@@ -53,9 +66,13 @@ var init = function (window) {
            
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
-            
+               for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i])
+                game.checkCirclePosition(circles[i])
+            }
         }
+          
+        
     
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -68,7 +85,15 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            
+             if (circle.x < 0) {
+                circle.x = canvas.width;
+            }
+            if (circle.y > canvas.height) {
+                circle.y = 0;
+            }
+            if (circle.y < 0) {
+                circle.y = canvas.height;
+            }
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
 
